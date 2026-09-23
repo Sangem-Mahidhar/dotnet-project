e4e47e3545f1aac79a6b96b74de3ef44778511ed
Sangem Mahidhar <sangem.mahidhar07@gmail.com>
2026-09-23 18:54:31 +0530

    commit-8

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+   required_providers {
+     azurerm= {
+        source = "hashicorp/azurerm"
+        version = ">=4.0.0"
+     }
+   }
+
+
+   backend "azurerm" {
+        resource_group_name  = "NetflixRG"
+        storage_account_name = "netflixsa2001"
+        container_name       = "new"
+        key                  = "terraform.tfstate"
+   }
+}
+
+
+provider "azurerm" {
+  features {}
+}
