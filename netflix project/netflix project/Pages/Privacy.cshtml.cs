af93aeb8833de05a500e20b0b6fe65978ecd4e31
Sangem Mahidhar <sangem.mahidhar07@gmail.com>
2026-09-23 14:23:07 +0530

    initial commit

---
+++ netflix project/netflix project/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace netflix_project.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
